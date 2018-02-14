d590bf08f1c6c4cd9dfe57d22182d0713a900d93
Daniel Ramos <danielrz@protonmail.com>
2018-02-14 01:36:40 -0300

    Update "config not found" snapshot

--- tests/snapshots/snap_test_config.py
+++ tests/snapshots/snap_test_config.py
@@ -16,5 +16,5 @@ snapshots['test_config_specifier 1'] = '''mylib==1.0.0
 snapshots['test_config_invalid_format 1'] = '''Invalid .pipwrc
 '''
 
-snapshots['test_config_not_found 1'] = '''Config file `REMOVED_FILEPATH` not found
+snapshots['test_config_not_found 1'] = '''Config file "REMOVED_FILEPATH" not found
 '''
